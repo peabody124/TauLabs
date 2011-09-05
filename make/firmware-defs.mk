0abbb0e411b0e3659539a084e31c081f2ef6d879
James Cotton <peabody124@gmail.com>
2011-09-04 20:10:37 -0500

    OpenOCD 0.5.0 support for ESC

--- make/firmware-defs.mk
+++ make/firmware-defs.mk
@@ -3,7 +3,7 @@ TCHAIN_PREFIX ?= arm-none-eabi-
 
 # Define toolchain component names.
 CC      = $(TCHAIN_PREFIX)gcc
-CXX     = $(TCHAIN_PREFIX)g++
+CPP     = $(TCHAIN_PREFIX)g++
 AR      = $(TCHAIN_PREFIX)ar
 OBJCOPY = $(TCHAIN_PREFIX)objcopy
 OBJDUMP = $(TCHAIN_PREFIX)objdump
@@ -23,41 +23,34 @@ else
 	quote =
 endif
 
-# Add a board designator to the terse message text
-ifeq ($(ENABLE_MSG_EXTRA),yes)
-	MSG_EXTRA := [$(BUILD_TYPE)|$(BOARD_SHORT_NAME)]
-else
-	MSG_BOARD :=
-endif
-
 # Define Messages
 # English
-MSG_FORMATERROR      = ${quote} Can not handle output-format${quote}
-MSG_MODINIT          = ${quote} MODINIT   $(MSG_EXTRA) ${quote}
-MSG_SIZE             = ${quote} SIZE      $(MSG_EXTRA) ${quote}
-MSG_LOAD_FILE        = ${quote} BIN/HEX   $(MSG_EXTRA) ${quote}
-MSG_BIN_OBJ          = ${quote} BINO      $(MSG_EXTRA) ${quote}
-MSG_STRIP_FILE       = ${quote} STRIP     $(MSG_EXTRA) ${quote}
-MSG_EXTENDED_LISTING = ${quote} LIS       $(MSG_EXTRA) ${quote}
-MSG_SYMBOL_TABLE     = ${quote} NM        $(MSG_EXTRA) ${quote}
-MSG_LINKING          = ${quote} LD        $(MSG_EXTRA) ${quote}
-MSG_COMPILING        = ${quote} CC        ${MSG_EXTRA} ${quote}
-MSG_COMPILING_ARM    = ${quote} CC-ARM    $(MSG_EXTRA) ${quote}
-MSG_COMPILINGCXX     = ${quote} CXX       $(MSG_EXTRA) ${quote}
-MSG_COMPILINGCXX_ARM = ${quote} CXX-ARM   $(MSG_EXTRA) ${quote}
-MSG_ASSEMBLING       = ${quote} AS        $(MSG_EXTRA) ${quote}
-MSG_ASSEMBLING_ARM   = ${quote} AS-ARM    $(MSG_EXTRA) ${quote}
-MSG_CLEANING         = ${quote} CLEAN     $(MSG_EXTRA) ${quote}
-MSG_ASMFROMC         = ${quote} AS(C)     $(MSG_EXTRA) ${quote}
-MSG_ASMFROMC_ARM     = ${quote} AS(C)-ARM $(MSG_EXTRA) ${quote}
-MSG_PYMITEINIT       = ${quote} PY        $(MSG_EXTRA) ${quote}
-MSG_INSTALLING       = ${quote} INSTALL   $(MSG_EXTRA) ${quote}
-MSG_TLFIRMWARE       = ${quote} TLFW      $(MSG_EXTRA) ${quote}
-MSG_FWINFO           = ${quote} FWINFO    $(MSG_EXTRA) ${quote}
-MSG_JTAG_PROGRAM     = ${quote} JTAG-PGM  $(MSG_EXTRA) ${quote}
-MSG_JTAG_WIPE        = ${quote} JTAG-WIPE $(MSG_EXTRA) ${quote}
-MSG_PADDING          = ${quote} PADDING   $(MSG_EXTRA) ${quote}
-MSG_FLASH_IMG        = ${quote} FLASH_IMG $(MSG_EXTRA) ${quote}
+MSG_FORMATERROR      := ${quote} Can not handle output-format${quote}
+MSG_MODINIT          := ${quote} MODINIT   ${quote}
+MSG_SIZE             := ${quote} SIZE      ${quote}
+MSG_LOAD_FILE        := ${quote} BIN/HEX   ${quote}
+MSG_BIN_OBJ          := ${quote} BINO      ${quote}
+MSG_STRIP_FILE       := ${quote} STRIP     ${quote}
+MSG_EXTENDED_LISTING := ${quote} LIS       ${quote}
+MSG_SYMBOL_TABLE     := ${quote} NM        ${quote}
+MSG_LINKING          := ${quote} LD        ${quote}
+MSG_COMPILING        := ${quote} CC        ${quote}
+MSG_COMPILING_ARM    := ${quote} CC-ARM    ${quote}
+MSG_COMPILINGCPP     := ${quote} CXX       ${quote}
+MSG_COMPILINGCPP_ARM := ${quote} CXX-ARM   ${quote}
+MSG_ASSEMBLING       := ${quote} AS        ${quote}
+MSG_ASSEMBLING_ARM   := ${quote} AS-ARM    ${quote}
+MSG_CLEANING         := ${quote} CLEAN     ${quote}
+MSG_ASMFROMC         := ${quote} AS(C)     ${quote}
+MSG_ASMFROMC_ARM     := ${quote} AS(C)-ARM ${quote}
+MSG_PYMITEINIT       := ${quote} PY        ${quote}
+MSG_INSTALLING       := ${quote} INSTALL   ${quote}
+MSG_OPFIRMWARE       := ${quote} OPFW      ${quote}
+MSG_FWINFO           := ${quote} FWINFO    ${quote}
+MSG_JTAG_PROGRAM     := ${quote} JTAG-PGM  ${quote}
+MSG_JTAG_WIPE        := ${quote} JTAG-WIPE ${quote}
+MSG_JTAG_RESET       := ${quote} JTAG-RST  ${quote}
+MSG_JTAG_SAFEBOOT    := ${quote} JTAG-SAFE ${quote}
 
 toprel = $(subst $(realpath $(TOP))/,,$(abspath $(1)))
 
@@ -120,7 +113,7 @@ endef
 #  $(1) = path to bin file
 #  $(2) = boardtype in hex
 #  $(3) = board revision in hex
-define TLFW_TEMPLATE
+define OPFW_TEMPLATE
 FORCE:
 
 $(1).firmwareinfo.c: $(1) $(TOP)/make/templates/firmwareinfotemplate.c FORCE
@@ -131,13 +124,12 @@ $(1).firmwareinfo.c: $(1) $(TOP)/make/templates/firmwareinfotemplate.c FORCE
 		--outfile=$$@ \
 		--image=$(1) \
 		--type=$(2) \
-		--revision=$(3) \
-		--uavodir=$(TOP)/shared/uavobjectdefinition
+		--revision=$(3)
 
 $(eval $(call COMPILE_C_TEMPLATE, $(1).firmwareinfo.c))
 
-$(OUTDIR)/$(notdir $(basename $(1))).tlfw : $(1) $(1).firmwareinfo.bin
-	@echo $(MSG_TLFIRMWARE) $$(call toprel, $$@)
+$(OUTDIR)/$(notdir $(basename $(1))).opfw : $(1) $(1).firmwareinfo.bin
+	@echo $(MSG_OPFIRMWARE) $$(call toprel, $$@)
 	$(V1) cat $(1) $(1).firmwareinfo.bin > $$@
 endef
 
@@ -145,7 +137,7 @@ endef
 define ASSEMBLE_TEMPLATE
 $(OUTDIR)/$(notdir $(basename $(1))).o : $(1)
 	@echo $(MSG_ASSEMBLING) $$(call toprel, $$<)
-	$(V1) $(CC) -c $(THUMB) $$(ASFLAGS) $$< -o $$@
+	$(V1) $(CC) -c -mthumb $$(ASFLAGS) $$< -o $$@
 endef
 
 # Assemble: create object files from assembler source files. ARM-only
@@ -159,7 +151,7 @@ endef
 define COMPILE_C_TEMPLATE
 $(OUTDIR)/$(notdir $(basename $(1))).o : $(1)
 	@echo $(MSG_COMPILING) $$(call toprel, $$<)
-	$(V1) $(CC) -c $(THUMB) $$(CFLAGS) $$(CONLYFLAGS) $$< -o $$@
+	$(V1) $(CC) -c -mthumb $$(CFLAGS) $$(CONLYFLAGS) $$< -o $$@
 endef
 
 # Compile: create object files from C source files. ARM-only
@@ -170,17 +162,17 @@ $(OUTDIR)/$(notdir $(basename $(1))).o : $(1)
 endef
 
 # Compile: create object files from C++ source files.
-define COMPILE_CXX_TEMPLATE
+define COMPILE_CPP_TEMPLATE
 $(OUTDIR)/$(notdir $(basename $(1))).o : $(1)
-	@echo $(MSG_COMPILINGCXX) $$(call toprel, $$<)
-	$(V1) $(CXX) -c $(THUMB) $$(CFLAGS) $$(CPPFLAGS) $$(CXXFLAGS) $$< -o $$@
+	@echo $(MSG_COMPILINGCPP) $$(call toprel, $$<)
+	$(V1) $(CC) -c -mthumb $$(CFLAGS) $$(CPPFLAGS) $$< -o $$@
 endef
 
 # Compile: create object files from C++ source files. ARM-only
-define COMPILE_CXX_ARM_TEMPLATE
+define COMPILE_CPP_ARM_TEMPLATE
 $(OUTDIR)/$(notdir $(basename $(1))).o : $(1)
-	@echo $(MSG_COMPILINGCXX_ARM) $$(call toprel, $$<)
-	$(V1) $(CPP) -c $$(CFLAGS) $$(CPPFLAGS) $$(CXXFLAGS) $$< -o $$@
+	@echo $(MSG_COMPILINGCPP_ARM) $$(call toprel, $$<)
+	$(V1) $(CC) -c $$(CFLAGS) $$(CPPFLAGS) $$< -o $$@
 endef
 
 # Link: create ELF output file from object files.
@@ -191,25 +183,14 @@ define LINK_TEMPLATE
 .PRECIOUS : $(2)
 $(1):  $(2)
 	@echo $(MSG_LINKING) $$(call toprel, $$@)
-	$(V1) $(CC) $(THUMB) $$(CFLAGS) $(2) --output $$@ $$(LDFLAGS)
-endef
-
-# Link: create ELF output file from object files.
-#   $1 = elf file to produce
-#   $2 = list of object files that make up the elf file
-define LINK_CXX_TEMPLATE
-.SECONDARY : $(1)
-.PRECIOUS : $(2)
-$(1):  $(2)
-	@echo $(MSG_LINKING) $$(call toprel, $$@)
-	$(V1) $(CXX) $(THUMB) $$(CFLAGS) $(2) --output $$@ $$(LDFLAGS)
+	$(V1) $(CC) -mthumb $$(CFLAGS) $(2) --output $$@ $$(LDFLAGS)
 endef
 
 # Compile: create assembler files from C source files. ARM/Thumb
 define PARTIAL_COMPILE_TEMPLATE
 $($(1):.c=.s) : %.s : %.c
 	@echo $(MSG_ASMFROMC) $$(call toprel, $$<)
-	$(V1) $(CC) $(THUMB) -S $$(CFLAGS) $$(CONLYFLAGS) $$< -o $$@
+	$(V1) $(CC) -mthumb -S $$(CFLAGS) $$(CONLYFLAGS) $$< -o $$@
 endef
 
 # Compile: create assembler files from C source files. ARM only
@@ -222,8 +203,7 @@ endef
 # $(1) = Name of binary image to write
 # $(2) = Base of flash region to write/wipe
 # $(3) = Size of flash region to write/wipe
-# $(4) = OpenOCD JTAG interface configuration file to use
-# $(5) = OpenOCD configuration file to use
+# $(4) = OpenOCD configuration file to use
 define JTAG_TEMPLATE
 # ---------------------------------------------------------------------------
 # Options for OpenOCD flash-programming
@@ -236,8 +216,7 @@ OOCD_EXE ?= openocd
 OOCD_JTAG_SETUP  = -d0
 # interface and board/target settings (using the OOCD target-library here)
 OOCD_JTAG_SETUP += -s $(TOP)/flight/Project/OpenOCD
-OOCD_JTAG_SETUP += -f $(4) -f $(5)
-
+OOCD_JTAG_SETUP += -f foss-jtag.revb.cfg -f $(4)
 # initialize
 OOCD_BOARD_RESET = -c init
 # show the targets
@@ -263,6 +242,35 @@ wipe:
 		$$(OOCD_JTAG_SETUP) \
 		$$(OOCD_BOARD_RESET) \
 		-c "flash erase_address pad $(2) $(3)" \
+		-c "reset run" \
+		-c "shutdown"
+
+reset:
+	@echo $(MSG_JTAG_RESET) resetting device
+	$(V1) $(OOCD_EXE) \
+		$$(OOCD_JTAG_SETUP) \
+		$$(OOCD_BOARD_RESET) \
+		-c "reset run" \
+		-c "shutdown"
+
+# Enable PWR and BKP clocks (set RCC_APB1ENR[PWREN|BKPEN])
+OOCD_WRITE_BKPDR3 =  -c "mww 0x4002101C 0x18000000"
+# Enable writes to BKP registers (set PWR_CR[DBP] via bit op alias address)
+#
+# Direct register access would be:
+#    mww 0x40007000 0x00000100
+#
+# Direct _bit_ access is:
+#    Bit 8 in 0x40007000 = 0x42000000 + 0x7000 * 32 + 8 * 4 = 420E0020
+OOCD_WRITE_BKPDR3 += -c "mww 0x420E0020 0x00000001"
+# Set BR3 to max value to force a safe boot
+OOCD_WRITE_BKPDR3 += -c "mwh 0x40006C0C 0xFFFF"
+safeboot:
+	@echo $(MSG_JTAG_SAFEBOOT) forcing boot into safe mode
+	$(V1) $(OOCD_EXE) \
+		$$(OOCD_JTAG_SETUP) \
+		$$(OOCD_BOARD_RESET) \
+		$$(OOCD_WRITE_BKPDR3) \
 		-c "reset run" \
 		-c "shutdown"
 endef
