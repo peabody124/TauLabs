b87b69e6c6b44495c96fafb65ababa9ec2d396b9
James Cotton <peabody124@gmail.com>
2011-11-13 13:09:16 -0600

    Add the board-info file for ESC

--- make/boards/esc/board-info.mk
+++ make/boards/esc/board-info.mk
@@ -7,17 +7,16 @@ MCU                 := cortex-m3
 CHIP                := STM32F103CBT
 BOARD               := STM32F103CB_ESC_Rev1
 MODEL               := MD
-MODEL_SUFFIX        :=
+MODEL_SUFFIX        := 
 
-OPENOCD_CONFIG      := stm32f1x.cfg 
+OPENOCD_CONFIG      := stm32f1x.cfg
 
 # Note: These must match the values in link_$(BOARD)_memory.ld
 BL_BANK_BASE        := 0x08000000  # Start of bootloader flash
-BL_BANK_SIZE        := 0x00003000  # Should include BD_INFO region
-FW_BANK_BASE        := 0x08003000  # Start of firmware flash
-FW_BANK_SIZE        := 0x0001D000  # Should include FW_DESC_SIZE (116kb)
+BL_BANK_SIZE        := 0x00002000  # Should include BD_INFO region
+FW_BANK_BASE        := 0x08002000  # Start of firmware flash
+FW_BANK_SIZE        := 0x0001CC00  # Should include FW_DESC_SIZE
+EE_BANK_BASE        := 0x0801FC00  # EEPROM storage area
+EE_BANK_SIZE        := 0x00000400  # Size of EEPROM storage area
 
 FW_DESC_SIZE        := 0x00000064
-
-EF_BANK_BASE        := 0x08000000  # Start of entire flash image (usually start of bootloader as well)
-EF_BANK_SIZE        := 0x00020000  # Size of the entire flash image (from bootloader until end of firmware)
